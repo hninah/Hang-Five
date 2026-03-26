5bb85fbc1d7c546d13920cc88ed19eee5e42bf21
evanspro <156554921+caesium-55@users.noreply.github.com>
2026-03-25 23:27:32 -0600

    started background scrolling/switching

---
+++ Assets/Scripts/Managers/BackgroundManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    //public variables
+    public List<GameObject> backgrounds;
+
+    [Header("Change Background After These Cutscenes")]
+    public List<int> cutsceneChanges;
+
+    //private variables
+    private int nextChange;
+
+    //Start is called every time we return to Gameplay scene
+    void Start(){
+        
+        //check the cutscene manager for the current cutscene
+        int nextCutscene = CutsceneManager.Instance.getCurrentCutscene();
+        int changeCount = 0;
+
+        foreach (int checkpoint in cutsceneChanges){
+            //count how many changes we've passed
+            if ( checkpoint <= nextCutscene ){
+                ++changeCount;
+            }
+        }
+
+        //otherwise enable the correct background group
+        for ( int i = 0; i < backgrounds.Count; ++i){
+            
+            //set active if we've looped through the changes to this one
+            if (i == (changeCount - 1) % backgrounds.Count){
+                backgrounds[i].SetActive(true);
+            }
+            //otherwise disable it
+            else{
+                backgrounds[i].SetActive(false);
+            }
+        }
+
+    }
+
+}
